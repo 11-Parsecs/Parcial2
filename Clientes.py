3275d1623c7334ad8f0dd1f99b33adf2dceefcad
Juan <juanpaguevara11@gmail.com>
2022-05-07 22:29:35 -0500

    Primera parte hecha

--- Clientes.py
+++ Clientes.py
@@ -1,5 +1,31 @@
-Txt="Ingrese {}"
-Txt1="la cantidad de productos que va a vender: "
-Txt2="el nombre de cada producto: "
-Txt3="el precio de cada uno: "
-Txt4="el descuento hecho si se compran más de diez: "
+Txt0="Ingrese {}"
+
+Txt1 ="los productos que va a vender: "
+Txt2 ="el nombre del producto {}: "
+Txt3 ="el precio de cada uno: "
+Txt4 ="el descuento hecho si se compran más de diez: "
+
+Txt5 ="la cantidad de clientes le compraron hoy: "
+
+Txt6 ="Cliente {}:"
+Txt7 ="  Nombre: "
+Txt8 ="  Producto: "
+Txt9 ="  Cantidad: "
+
+Txt10="{:10s} {:5s} {:3d}  ${:7.2f}"
+Txt11='''
+Valor:         ${}
+Valor con IVA: ${}
+Descuento:     {}%
+Total:         ${}
+'''
+
+N=input(Txt0.format(Txt1))
+
+n=[]
+p=0
+
+for i in range(1,eval(N)+1):
+    p+=1
+    n.append(input(Txt0.format(Txt2.format(p))))
+    print(n)
